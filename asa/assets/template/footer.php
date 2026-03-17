093d12f455792647edb548550d2a974fd0f547b6
casperplays1337YT <74802499+casperplays1337YT@users.noreply.github.com>
2026-03-17 22:47:52 +0100

    Delete asa directory

--- asa/assets/template/footer.php
+++
@@ -1,4 +0,0 @@
-</div> <!-- container slut -->
-<footer>© 2026 Asatro Hub</footer>
-</body>
-</html>
--- asa/kalender.php
+++
@@ -1,7 +0,0 @@
-<?php include 'assets/template/header.php'; ?>
-<h2>Högtider</h2>
-<div class="card"><b>Midvinterblot</b> - 21 Dec: Hyllar Oden och förfäder.</div>
-<div class="card"><b>Vårblot</b> - Mars: Markerar vårens ankomst, fruktbarhet.</div>
-<div class="card"><b>Sommarblot</b> - Juni: Solens kraft, skördeförberedelser.</div>
-<div class="card"><b>Skördefest</b> - Sept: Tack till naturen, avslut på året.</div>
-<?php include 'assets/template/footer.php'; ?>
